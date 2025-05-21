8fcca314021af8c110d8cd997c04d987b7a9fb00
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-05-22 05:11:16 +0900

    ユーザー登録APIの作成（２）

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,4 +1,17 @@
+import prisma from "@/lib/prisma";
+
 export async function POST(request: Request) {
-    const res = await request.json()
-    return Response.json({ res })
-  }
+  const res = await request.json();
+
+  const { username, email, password } = res;
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+
+  return Response.json({ user });
+}
