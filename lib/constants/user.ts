d691079b3aba9dbf219b772a0ed2624bd8b65aac
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-05-24 00:40:30 +0900

    Prisma Clientの例外発生について

---
+++ lib/constants/user.ts
@@ -0,0 +1,5 @@
+export const USER_FIELD_NAMES = {
+  username: "ユーザー名",
+  email: "メールアドレス",
+  password: "パスワード",
+};
--- prisma/api/handler.ts
+++ prisma/api/handler.ts
@@ -1,11 +1,24 @@
+import { USER_FIELD_NAMES } from "@/lib/constants/user";
+import { Prisma } from "@prisma/client";
+
 type ApiHandler = (request: Request) => Promise<Response>;
 
 export const withErrorHandler = (handler: ApiHandler) => {
   return async (req: Request) => {
     try {
       return await handler(req);
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      console.error(err);
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        return Response.json(
+          {
+            error: getPrismaErrorMessage(err),
+          },
+          {
+            status: 500,
+          }
+        );
+      }
       return Response.json(
         {
           error: "予期せぬエラーが発生しました",
@@ -16,4 +29,22 @@ export const withErrorHandler = (handler: ApiHandler) => {
       );
     }
   };
+};
+
+export const getPrismaErrorMessage = (
+  error: Prisma.PrismaClientKnownRequestError
+) => {
+  switch (error.code) {
+    case "P2002":
+      const fieldnames =
+        (error.meta?.target as string[])
+          .map(
+            (name) =>
+              USER_FIELD_NAMES[name as keyof typeof USER_FIELD_NAMES] ?? name
+          )
+          .join(",") ?? "特定のフィールド";
+      return `${fieldnames}はすでに登録されている情報です`;
+    default:
+      return "予期せぬエラーが発生しました";
+  }
 };
