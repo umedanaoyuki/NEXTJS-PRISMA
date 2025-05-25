a50c826075cff9d7f62b987c102149cc4dcc0a47
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-05-25 16:48:04 +0900

    feat: 認証情報の取得部分の共通化

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,20 +1,10 @@
-import { getAuthToken, verifyJWT } from "@/lib/api/auth";
+import { withAuth } from "@/lib/api/handler";
 import { validateRequest } from "@/lib/api/validation";
 import prisma from "@/lib/prisma";
-import { withErrorHandler } from "@/prisma/api/handler";
 import ArticleCreateManyUserInputSchema from "@/prisma/generated/zod/inputTypeSchemas/ArticleCreateManyUserInputSchema";
 
-export const POST = withErrorHandler(async (request: Request) => {
-  const token = await getAuthToken();
-
-  if (token == null) {
-    return Response.json({ error: "認証が必要です" }, { status: 401 });
-  }
-
-  const { userId } = await verifyJWT(token);
-
+export const POST = withAuth(async (request: Request, userId: number) => {
   const res = await request.json();
-
   const bodyValidation = validateRequest(res, ArticleCreateManyUserInputSchema);
 
   if (!bodyValidation.success) {
--- lib/api/handler.ts
+++ lib/api/handler.ts
@@ -1,3 +1,6 @@
+import { handleClientScriptLoad } from "next/script";
+import { getAuthToken, verifyJWT } from "./auth";
+
 type ApiHandler = (request: Request) => Promise<Response>;
 
 export const withErrorHandler = (handler: ApiHandler) => {
@@ -8,4 +11,24 @@ export const withErrorHandler = (handler: ApiHandler) => {
       console.error(error);
     }
   };
-}
+};
+
+// TODO: getPrismaErrorMessage
+
+type AuthenticatedHandler = (
+  request: Request,
+  uerId: number
+) => Promise<ReportingObserver>;
+
+export const withAuth = (handler: AuthenticatedHandler) => {
+  return withErrorHandler(async (req: Request) => {
+    const token = await getAuthToken();
+
+    if (token == null) {
+      return Response.json({ error: "認証が必要です" }, { status: 401 });
+    }
+
+    const { userId } = await verifyJWT(token);
+    return handler(req, userId);
+  });
+};
